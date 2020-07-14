4d8e6472f3ee669633b93b010a91369c36c00ea7
naimul-haque-sagar <naimulhaquesagar@gmail.com>
2020-07-14 14:54:24 +0600

    swagger added

--- src/main/java/rest/SpringRestDataMongoDbApplication.java
+++ src/main/java/rest/SpringRestDataMongoDbApplication.java
@@ -3,10 +3,13 @@ package rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+import rest.configuration.SwaggerConfiguration;
 
 @SpringBootApplication
+@Import(SwaggerConfiguration.class)
 public class SpringRestDataMongoDbApplication {
 
 	public static void main(String[] args) {
